f188c35c5fd2d9da216894107c6fa1ce4e2476bc
unknown <jeffmichaelgomez@gmail.com>
2025-09-15 06:20:39 +0800

    feat: create iam roles for bkt access

---
+++ envs/prod/iam_roles.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "prod_bucket_access" {
+  name = "ProdBucketAccess"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::135878023383:root"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "prod_bucket_policy" {
+  name   = "ProdBucketAccessPolicy"
+  policy = file("file(${path.root}/../../policies/prod/prod_bucket_user_policy.json)")
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.prod_bucket_access.name
+  policy_arn = aws_iam_policy.prod_bucket_policy.arn
+}
