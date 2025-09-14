144199c501b3b70896c4c8afe5081fdeecd72672
unknown <jeffmichaelgomez@gmail.com>
2025-09-14 09:46:40 +0800

    test: dev tf plan

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "s3-tf-backend-135878023383"
-    key            = "dev/terraform.tfstate"
+    key            = "terraform.tfstate"
+    workspace_key_prefix = "dev" 
     region         = "ap-southeast-2"
     dynamodb_table = "tf-state-locks"
     encrypt        = true
--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "s3-tf-backend-135878023383"
     key            = "prod/terraform.tfstate"
+    workspace_key_prefix = "prod" 
     region         = "ap-southeast-2"
     dynamodb_table = "tf-state-locks"
     encrypt        = true
