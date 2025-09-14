02c404ba11e782305e615775125f2b6745ee89f9
unknown <jeffmichaelgomez@gmail.com>
2025-09-14 10:06:49 +0800

    fix: tf fmt

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     bucket               = "s3-tf-backend-135878023383"
-    key                  = "terraform.tfstate"
-    workspace_key_prefix = "dev"
+    key                  = "dev/terraform.tfstate"
     region               = "ap-southeast-2"
     dynamodb_table       = "tf-state-locks"
     encrypt              = true
