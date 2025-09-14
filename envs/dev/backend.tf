7f90dadb2565f6583fb30a665ba28bf51d0999f8
unknown <jeffmichaelgomez@gmail.com>
2025-09-14 10:09:03 +0800

    test: ci aws resource

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket               = "s3-tf-backend-135878023383"
-    key                  = "dev/terraform.tfstate"
-    region               = "ap-southeast-2"
-    dynamodb_table       = "tf-state-locks"
-    encrypt              = true
+    bucket         = "s3-tf-backend-135878023383"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "tf-state-locks"
+    encrypt        = true
   }
 }
